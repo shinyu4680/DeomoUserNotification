0a59f26e3489b9cf8693c3e469a9f4698b61e75d
張心禹 <kevin@kevins-macbook-pro.local>
2018-06-30 21:00:33 +0800

    version 1

--- DeomoUserNotification/AppDelegate.swift
+++ DeomoUserNotification/AppDelegate.swift
@@ -7,15 +7,41 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let center = UNUserNotificationCenter.current()
+    
+    // delegate for receiving or delivering notification
+    let notificationDelegate = DemoNotificationDelegate()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        center.delegate = notificationDelegate
+        
+        // MARK: set authorization
+        let options: UNAuthorizationOptions = [.badge, .sound, .alert]
+        center.getNotificationSettings { ( settings ) in
+            switch settings.authorizationStatus {
+            case .notDetermined:
+                self.center.requestAuthorization(options: options) {
+                    (granted, error) in
+                    if !granted {
+                        print("Something went wrong")
+                    }
+                }
+            case .authorized:
+                DispatchQueue.main.async(execute: {
+                    UIApplication.shared.registerForRemoteNotifications()
+                })
+            case .denied:
+                print("cannot use notifications cuz the user has denied permissions")
+            }
+        }
+        
         return true
     }
 
@@ -31,10 +57,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        UIApplication.shared.applicationIconBadgeNumber = 0
+        center.removeAllDeliveredNotifications()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
--- DeomoUserNotification/ViewController.swift
+++ DeomoUserNotification/ViewController.swift
@@ -7,19 +7,134 @@
 //
 
 import UIKit
+import UserNotifications
+
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var notificationTitileTextField: UITextField!
+    @IBOutlet weak var notificationBodyTextField: UITextField!
+    @IBOutlet weak var triggerTimePicker: UIDatePicker!
+    
+    
+    let app = UIApplication.shared.delegate as! AppDelegate
+    var center: UNUserNotificationCenter?
+    
+    let snoozeAction = UNNotificationAction(identifier: "SnoozeAction",
+                                            title: "Snooze", options: [.authenticationRequired])
+    let deleteAction = UNNotificationAction(identifier: "DeleteAction",
+                                            title: "Delete", options: [.destructive])
+    
+    
+    @IBAction func sendNotificationBtnPressed(_ sender: UIButton) {
+        center?.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .authorized {
+                self.sendNotification()
+            }else if settings.authorizationStatus == .denied {
+                self.deniedAlert()
+            }else { return }
+        }
+    }
+    
+    func sendNotification() {
+        // Main thread checker
+        DispatchQueue.main.async {
+            let titleText = self.notificationTitileTextField.text
+            let bodyText = self.notificationBodyTextField.text
+            
+            if titleText != nil && bodyText != nil {
+                let customID = titleText!
+                let identifier = "DemoNotifiaction" + customID
+                let content = UNMutableNotificationContent()
+                content.title = titleText!
+                content.body = bodyText!
+                content.sound = UNNotificationSound.default()
+                content.badge = NSNumber(integerLiteral: UIApplication.shared.applicationIconBadgeNumber + 1)
+                content.categoryIdentifier = "DemoNotificationCategory"
+                let timeInterval = self.triggerTimePicker.countDownDuration
+                // for testing the timeInterval set to 10 sec
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+                self.center?.add(request, withCompletionHandler: {(error) in
+                    if let error = error {
+                        print("\(error)")
+                    }else {
+                        print("successed")
+                    }
+                })
+            } else if titleText == nil || bodyText == nil{
+                self.emptyAlert()
+            }
+        }
+        
+        
+    }
+    
+    func emptyAlert() {
+        let controller = UIAlertController(title: "Warning", message: "Title or Body can't be empty!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        controller.addAction(action)
+        
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    func deniedAlert() {
+        let useNotificationsAlertController = UIAlertController(title: "Turn on notifications", message: "This app needs notifications turned on for the best user experience \n ", preferredStyle: .alert)
+        
+        // go to setting alert action
+        let goToSettingsAction = UIAlertAction(title: "Go to settings", style: .default, handler: { (action) in
+            guard let settingURL = URL(string: UIApplicationOpenSettingsURLString) else { return }
+            if UIApplication.shared.canOpenURL(settingURL) {
+                UIApplication.shared.open(settingURL, completionHandler: { (success) in
+                    print("Settings opened: \(success)")
+                })
+            }
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        useNotificationsAlertController.addAction(goToSettingsAction)
+        useNotificationsAlertController.addAction(cancelAction)
+        
+        self.present(useNotificationsAlertController, animated: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let category = UNNotificationCategory(identifier: "DemoNotificationCategory",
+                                              actions: [snoozeAction,deleteAction],
+                                              intentIdentifiers: [], options: [])
+        center = app.center
+        center?.setNotificationCategories([category])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func checkPendingNotification(_ sender: Any) {
+        center?.getPendingNotificationRequests(completionHandler: {requests in
+            for request in requests{
+                print(request)
+            }
+        })
+        
+        /*
+         center?.getDeliveredNotificationRequest(completionHandler: {requests in
+            for request in requests{
+                print(request)
+            }
+         })
+        center?.removeAllPendingNotificationRequests()
+        center?.removePendingNotificationRequests(withIdentifiers: ["String"])
+        center?.removeAllDeliveredNotifications()
+        center?.removeDeliveredNotifications(withIdentifiers: ["String"])
+        */
+    }
 
 }
+
 
